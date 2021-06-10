10d54f6ae3937d6b2816e084698f65bafa053723
Cristina <cristoshiwassaki@gmail.com>
2021-06-09 23:27:20 -0300

    App com uso de Sass

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import './App.css';
+import './App.scss';
 import Lista from './componentes/listaCard'
 import Status from './componentes/paginaInfo'
 
